f88b861223dfd0de2fff81c5958790b946a69bd3
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-04-06 10:26:40 +0200

    Add sign in page

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,12 @@
 import Navigation from 'components/Navigation';
 
 const Layout = ({ children }) => (
-  <div className="flex flex-col justify-start">
+  <div className="flex flex-col min-h-screen">
     <Navigation />
 
-    <main className="container mx-auto px-3 lg:px-0 py-5 lg:py-10">{children}</main>
+    <main className="container mx-auto flex-grow flex flex-col px-3 lg:px-0 py-5 lg:py-10">
+      {children}
+    </main>
 
     <footer className="container mx-auto mt-auto">© {new Date().getFullYear()}</footer>
   </div>
--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -13,20 +13,25 @@ const Navigation = () => {
     <nav className="bg-white border-b border-gray-200">
       <div className="container flex items-center flex-wrap mx-auto p-3 lg:px-0 lg:py-6">
         <Link href="/">
-          <a className="text-xl font-bold tracking-wide mr-6 lg:text-2xl">Jobify</a>
+          <a className="text-xl font-bold tracking-wide mr-6 lg:text-2xl mr-auto">Jobify</a>
         </Link>
         <button
           onClick={toggleMenu}
-          className="py-2 px-3 text-black rounded ml-auto outline-none bg-gray-100 lg:hidden">
+          className="py-2 px-3 text-black rounded outline-none bg-gray-100 lg:hidden">
           Menu
         </button>
         <ul
-          className={classNames('w-full lg:inline-flex lg:flex-grow lg:w-auto', {
+          className={classNames('w-full lg:inline-flex lg:w-auto', {
             hidden: !isOpen
           })}>
-          <li className="w-full flex lg:inline-flex lg:ml-auto lg:w-auto">
+          <li className="w-full flex lg:inline-flex lg:ml-3 lg:w-auto">
             <Link href="/">
               <a className="w-full py-2 lg:w-auto lg:py-0">Submit offer</a>
+            </Link>
+          </li>
+          <li className="w-full flex lg:inline-flex lg:ml-3 lg:w-auto">
+            <Link href="/login">
+              <a className="w-full py-2 lg:w-auto lg:py-0">Login</a>
             </Link>
           </li>
         </ul>
