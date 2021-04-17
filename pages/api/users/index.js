f2161b56350f5de4050dba81b26440e951b957c5
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-04-17 14:24:41 +0200

    Add backend for creating new users

---
+++ pages/api/users/index.js
@@ -0,0 +1,20 @@
+import createUser from 'services/users/create';
+
+export default async (req, res) => {
+  switch (req.method) {
+    case 'POST': {
+      try {
+        const payload = req.body;
+        const user = await createUser(payload);
+
+        res.status(200).json({ status: 'created', user });
+      } catch (error) {
+        res.status(422).json({ status: 'not_created', error });
+      }
+
+      break;
+    }
+    default:
+      res.status(400);
+  }
+};
