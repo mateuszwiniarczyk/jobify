0c744ab629791c8d645a38af7ab64d5898814032
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-04-17 13:21:43 +0200

    Add fallback and revalidate for offer pages

---
+++ services/offers/get.js
@@ -0,0 +1,13 @@
+import airDB from 'services/airtableClient';
+
+const get = async (id) => {
+  const offers = await airDB('offers')
+    .select({ filterByFormula: `id=${id}` })
+    .firstPage();
+
+  if (offers && offers[0]) {
+    return offers[0].fields;
+  }
+};
+
+export default get;
