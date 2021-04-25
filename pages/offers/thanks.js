2d1fd21b46fc61dfb5c6c660babe65356dc00135
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-04-25 11:37:43 +0200

    Create thanks page after adding offer

--- pages/offers/thanks.js
+++ pages/offers/thanks.js
@@ -1,9 +1,22 @@
 import Layout from 'components/Layout';
+import Link from 'next/link';
 
 const Thanks = () => {
   return (
     <Layout>
-      <h1>Thanks</h1>
+      <div className="my-auto flex flex-col items-center justify-center gap-5 text-center">
+        <h1 className="text-2xl font-medium leading-none text-gray-700">
+          Thanks for submitting new offer.
+        </h1>
+        <p className="text-xl text-gray-600 leading-none">
+          Upon positive verification, it will show in our listing
+        </p>
+        <Link href="/">
+          <a className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+            Go to homepage
+          </a>
+        </Link>
+      </div>
     </Layout>
   );
 };
